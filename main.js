b853c769f73efd41ca7eab0dc8a582b395a63b8c
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-04 22:42:38 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log('connected')
+
+const getAllBtn = document.querySelector('#all')
+const charBtns = document.querySelectorAll('.char-btns')
+const ageForm = document.querySelector('#age-form')
+const ageInput = document.querySelector('#age-input')
+const createForm = document.querySelector('#create-form')
+const newFirstInput = document.querySelector('#first')
+const newLastInput = document.querySelector('#last')
+const newGenderDropDown = document.querySelector('select')
+const newAgeInput = document.querySelector('#age')
+const newLikesText = document.querySelector('textarea')
+const charContainer = document.querySelector('section')
+
+// const baseURL = 
+
+function createCharacterCard(char) {
+  let charCard = document.createElement('div')
+  charCard.innerHTML = `<h3>${char.firstName} ${char.lastName}</h3>
+  <p>gender: ${char.gender} | age: ${char.age}</p>
+  <h4>Likes</h4>
+  <ul>
+    <li>${char.likes[0]}</li>
+    <li>${char.likes[1]}</li>
+    <li>${char.likes[2]}</li>
+  </ul>`
+
+  charContainer.appendChild(charCard)
+}
+
+function clearCharacters() {
+  charContainer.innerHTML = ``
+}
